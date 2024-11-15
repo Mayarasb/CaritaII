fe83ecaae99992171735033d90d1644dd75edaae
MayaraSb <mayarasbarros7@gmail.com>
2024-11-14 22:43:18 -0300

    Reactive Forms

--- src/app/componentes/campos-login/campos-login.component.ts
+++ src/app/componentes/campos-login/campos-login.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-campos-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule,CommonModule],
   templateUrl: './campos-login.component.html',
   styleUrl: './campos-login.component.css'
 })
 export class CamposLoginComponent {
+ myForm: FormGroup;
+
+ constructor(private fb: FormBuilder){
+this.myForm = this.fb.group({
+    email:['',Validators.required, Validators.email],
+    senha:['',Validators.required]
+  })
+ }
+
+onSubmit(){
+  console.log(this.myForm.value);
+}
 
 }
