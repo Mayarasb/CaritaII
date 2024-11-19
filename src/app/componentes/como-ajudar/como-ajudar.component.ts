030158fdd2d645d92596f7e3b4f01f99c38fed3c
MayaraSb <mayarasbarros7@gmail.com>
2024-11-19 20:16:06 -0300

    PInclusão Componente Como Ajudar

---
+++ src/app/componentes/como-ajudar/como-ajudar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-como-ajudar',
+  standalone: true,
+  imports: [],
+  templateUrl: './como-ajudar.component.html',
+  styleUrl: './como-ajudar.component.css'
+})
+export class ComoAjudarComponent {
+
+}
--- src/app/componentes/pagina-comoajudar/pagina-comoajudar.component.ts
+++ src/app/componentes/pagina-comoajudar/pagina-comoajudar.component.ts
@@ -5,11 +5,12 @@ import { ListaComponent } from '../lista/lista.component';
 import { CardDoepixComponent } from '../card-doepix/card-doepix.component';
 import { ParceirosComponent } from '../parceiros/parceiros.component';
 import { FooterComponent } from '../footer/footer.component';
+import { ComoAjudarComponent } from "../como-ajudar/como-ajudar.component";
 
 @Component({
   selector: 'app-pagina-comoajudar',
   standalone: true,
-  imports: [HeaderComponent,BannerComoajudarComponent,ListaComponent, CardDoepixComponent,ParceirosComponent,FooterComponent],
+  imports: [HeaderComponent, BannerComoajudarComponent, ListaComponent, CardDoepixComponent, ParceirosComponent, FooterComponent, ComoAjudarComponent],
   templateUrl: './pagina-comoajudar.component.html',
   styleUrl: './pagina-comoajudar.component.css'
 })
