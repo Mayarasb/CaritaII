a9d94e7d29a9342430e757fc0c88007a0880de48
Mayara <mayarasbarros7@gmail.com>
2024-11-25 17:53:38 -0300

    Integração do  Google Maps

--- src/app/componentes/como-ajudar/como-ajudar.component.ts
+++ src/app/componentes/como-ajudar/como-ajudar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { GoogleMapsComponent } from '../google-maps/google-maps.component';
 
 @Component({
   selector: 'app-como-ajudar',
   standalone: true,
-  imports: [],
+  imports: [GoogleMapsComponent],
   templateUrl: './como-ajudar.component.html',
   styleUrl: './como-ajudar.component.css'
 })
 export class ComoAjudarComponent {
 
+
+  
 }
